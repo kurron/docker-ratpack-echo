41671cda6f78682de60a902df3eb2b2db250f8fc
Ron Kurr <kurr@kurron.org>
2016-09-04 14:39:23 -0400

    Initial commit

---
+++ configuration-server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This needs to be customized for each machine
+CMD="docker run \
+       --detach \
+       --restart always \
+       --name configuration-server \
+       --net "host" \
+       --env SPRING_CLOUD_CONSUL_HOST=192.168.1.227 \
+       --env JVM_HEAP_MIN=128m \
+       --env JVM_HEAP_MAX=128m \
+       --env JVM_JMX_HOST=10.0.2.15 \
+       --env JVM_JMX_PORT=2020 \
+       --env JVM_JMX_RMI_PORT=2020 \
+       kurron/docker-spring-cloud-configuration-server:1.2.0"
+
+echo eval $CMD
+eval $CMD $*
